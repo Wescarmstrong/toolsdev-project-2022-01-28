4bc543d99dc4ab8a9b7628fb15f0907acbfaaac0
Wes <wescarmstrong@gmail.com>
2022-01-31 00:01:19 -0600

    Database data is drawn in HighChart/View

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,35 +4,44 @@ class HomeController < ApplicationController
   def index
 
     @BCHQTemp = BigCommerceHeadquarterTemp.all
-    puts @BCHQTemp.size
-    puts @BCHQTemp[0].date
-    puts @BCHQTemp[0].temp
+    # puts @BCHQTemp
+    # puts @BCHQTemp[0].date
+    # puts @BCHQTemp[0].temp
+
+    # Format Model data for use with Highcharts
+
+    chartArrayFormatted = []
+    dataCounter = 0
+    begin
+      # puts @BCHQTemp[dataCounter].date
+      # puts @BCHQTemp[dataCounter].temp
+
+      # Create an array for each date/temp record
+      singleHourTemp = []
+
+      #Convert Unix Time - seconds -> ms
+      alteredDateFormat = @BCHQTemp[dataCounter].date.to_i * 1000
+
+      singleHourTemp.push(alteredDateFormat, @BCHQTemp[dataCounter].temp)
+      chartArrayFormatted.push(singleHourTemp)
+
+      dataCounter = dataCounter + 1
+    end while dataCounter < @BCHQTemp.size
 
 
-
-
-
-    # getWeatherdata()
-
-    # example = BigCommerceHeadquarterTemp.new
-    # example.date = '2022-01-31 03'
-    # example.temp = 60
-    # example.save
-
-    # puts "Example DATA"
-    # puts example.date
-    # puts example.temp
-
+    @chartArrayFormatted = chartArrayFormatted
+    
+    getWeatherdata()
   end
 
   def getWeatherdata
     # Get Weather data via external API
-    uri = URI('https://api.worldweatheronline.com/premium/v1/past-weather.ashx?key=d13f831881e94157a6c11348223001&q=30.40,-97.84&date=2022-01-27&enddate=2022-01-28&tp=1&format=json')
+    uri = URI('https://api.worldweatheronline.com/premium/v1/past-weather.ashx?key=d13f831881e94157a6c11348223001&q=30.40,-97.84&date=2022-01-24&enddate=2022-01-28&tp=1&format=json')
     api_data = (Net::HTTP.get(uri))
 
     data = JSON[api_data]
 
-    puts "Other results are: "
+    puts "results are: "
     puts data["data"]["weather"][0]["hourly"][0]["time"]  #time - 0
     puts data["data"]["weather"][0]["hourly"][1]["time"]  #time - 300
     puts data["data"]["weather"][0]["hourly"][1]["tempF"] #tempF - 47 
