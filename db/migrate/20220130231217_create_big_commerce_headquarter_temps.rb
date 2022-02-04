d670f9bf8a8f2a6560911695f9e343d6a6553dd7
Wes <wescarmstrong@gmail.com>
2022-02-03 20:42:08 -0600

    corrected datetime for db date

--- db/migrate/20220130231217_create_big_commerce_headquarter_temps.rb
+++ db/migrate/20220130231217_create_big_commerce_headquarter_temps.rb
@@ -1,7 +1,7 @@
 class CreateBigCommerceHeadquarterTemps < ActiveRecord::Migration[5.2]
   def change
     create_table :big_commerce_headquarter_temps do |t|
-      t.timestamps :date
+      t.datetime :date
       t.integer :temp
 
       t.timestamps
